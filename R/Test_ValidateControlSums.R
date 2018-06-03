d07a7367963ef38037519d14598b080a371fdf07
kiwimic <michalsiwik@gmail.com>
2018-06-03 21:24:25 +0200

    Function to makes test of equal values easier, and to refactor code

---
+++ R/Test_ValidateControlSums.R
@@ -0,0 +1,19 @@
+
+# This is an function that convert character cols from dataframe to numeric.
+# You have to pass colnames to convert as argument colsToConv
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+Test_ValidateControlSums <- function(V1, V2, msg, onlyWarning = F) {
+  if (onlyWarning) {
+    if (V1 != V2) {
+      warning(sprintf(msg, V1, V2))
+    }
+
+  } else {
+    stop(sprintf(msg, V1, V2))
+  }
+
+}
