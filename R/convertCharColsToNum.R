cc9a846477c08f76cef6f2b1d4ed5441a707d070
kiwimic <michalsiwik@gmail.com>
2018-06-04 20:23:44 +0200

    Added documentation

--- R/convertCharColsToNum.R
+++ R/convertCharColsToNum.R
@@ -1,14 +1,14 @@
-
-
-
-# This is an function that convert character cols from dataframe to numeric.
-# You have to pass colnames to convert as argument colsToConv
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
 library(tibble)
+#' convertCharColsToNum
+#'
+#' @param df df of wchich cols you want to convert
+#' @param colsToConv if guess is False, you have to pass list of cols by names like c("K_10", "K_15")
+#' @param guess logical if guess is True, then function will guess automatically which columns have only numbers and have to be converted to numeric
+#'
+#' @return returns tibble (kind of data.frame) where cols which are only numeric (but class character), are converted to numeric
+#'
+#'
+#' @seealso \code{\link{as.tibble}}
 convertCharColsToNum <- function(df, colsToConv, guess = F) {
   if (!(is.data.frame(df) | is.tibble(df))) {
     stop(
