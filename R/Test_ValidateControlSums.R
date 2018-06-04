6ebd5a6ecaa7054ab1991908ae1f2229b42f6a21
kiwimic <michalsiwik@gmail.com>
2018-06-04 20:10:24 +0200

    Added roxygen skeleton and created documentation

--- R/Test_ValidateControlSums.R
+++ R/Test_ValidateControlSums.R
@@ -1,11 +1,14 @@
-
-# This is an function that convert character cols from dataframe to numeric.
-# You have to pass colnames to convert as argument colsToConv
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
+#' Test_ValidateControlSums test if two values are equal with '==' and returns error, or warning. This depends form onlyWarning param
+#'
+#' @param V1 First value - scalar
+#' @param V2 Second value - scalar
+#' @param msg Message formated to sprintf function
+#' @param onlyWarning param to choose if user want to only warn or error. Warning allows funcion to finish, error stops function
+#'
+#' @return error or warning with msg
+#'
+#'
+#' @seealso \code{\link{sprintf}}
 Test_ValidateControlSums <- function(V1, V2, msg, onlyWarning = F) {
   if (V1 != V2) {
     if (onlyWarning) {
