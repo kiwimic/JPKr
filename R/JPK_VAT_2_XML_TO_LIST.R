dabbcaa84ade6aa09dbdfd20c7569fdceece89ea
kiwimic <michalsiwik@gmail.com>
2018-06-04 19:10:14 +0200

    Added guess parameter to convertCharColsToNum to guess colls you want to convert :)

--- R/JPK_VAT_2_XML_TO_LIST.R
+++ R/JPK_VAT_2_XML_TO_LIST.R
@@ -7,6 +7,7 @@
 #SprzedazCtrl
 #ZakupyWiersz
 #ZakupyCtrl
+
 #
 
 
--- R/convertCharColsToNum.R
+++ R/convertCharColsToNum.R
@@ -1,4 +1,6 @@
 
+
+
 # This is an function that convert character cols from dataframe to numeric.
 # You have to pass colnames to convert as argument colsToConv
 #
@@ -7,13 +9,34 @@
 #   http://r-pkgs.had.co.nz/
 #
 library(tibble)
-convertCharColsToNum <- function(df, colsToConv) {
-  if (!(is.data.frame(df)|is.tibble(df))) {
-    stop(sprintf("Please pass data.frame or tibble as df argument Argument you just pass is class: %s", class(df)))
+convertCharColsToNum <- function(df, colsToConv, guess = F) {
+  if (!(is.data.frame(df) | is.tibble(df))) {
+    stop(
+      sprintf(
+        "Please pass data.frame or tibble as df argument Argument you just pass is class: %s",
+        class(df)
+      )
+    )
   }
 
-  df[, colsToConv] <- apply(df[, colsToConv], 2,   function(x) {
-    return(as.numeric(x))
-  })
+  if (guess) {
+    df2 <- suppressWarnings(as.tibble(lapply(df, as.numeric)))
+    sumOfNA <- apply(df2, 2, function(x) {
+      return(sum(is.na(x)))
+    })
+    sumOfNA == 0
+    df[,sumOfNA==0] <- as.tibble(lapply(df[,sumOfNA == 0], as.numeric))
+    df <- as.tibble(df)
+  } else {
+    df[, colsToConv] <- apply(df[, colsToConv], 2,   function(x) {
+      return(as.numeric(x))
+    })
+  }
   return(df)
 }
+
+
+
+################
+
+
