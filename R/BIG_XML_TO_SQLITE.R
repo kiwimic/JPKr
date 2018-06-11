4f8637a623302f596d974446e5799a8e73f5d51d
kiwimic <michalsiwik@gmail.com>
2018-06-11 21:23:49 +0200

    Add logic

---
+++ R/BIG_XML_TO_SQLITE.R
@@ -0,0 +1,6 @@
+###1. xml2::read_xml() ####
+###2. guessJpkType####
+###3. Read number of rows in tables####
+###4. Prepare sqlite base####
+###5. Read, add missing cols, write to db####
+###6. ####
