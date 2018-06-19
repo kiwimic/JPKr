4bcc4a5be55cbb2ec003dfde94f991f53775b974
kiwimic <michalsiwik@gmail.com>
2018-06-19 23:42:57 +0200

    DOdano funkcję BIG_XML_TO_SQLITE(a raczej jej szkielet... narazie to jest za wolne)

--- R/BIG_XML_TO_SQLITE.R
+++ R/BIG_XML_TO_SQLITE.R
@@ -4,3 +4,36 @@
 ###4. Prepare sqlite base####
 ###5. Read, add missing cols, write to db####
 ###6. ####
+
+
+BIG_XML_TO_SQLITE <- function(file_xml = "", file_SQL = "Plik_JPK.sqlite") {
+
+  ##JPK_FA#####
+  xml2::read_xml(file_xml, encoding = "UTF-8") %>%
+    xml_find_first("//d1:FakturaCtrl") %>%
+    as_list() %>%
+    unlist() %>%
+    t() %>%
+    as.tibble() %>%
+    select(LiczbaFaktur) %>%
+    unlist() %>%
+    unname() %>%
+    as.numeric() -> JPK_FA_LiczbaFaktur
+
+  xml2::read_xml(file_xml, encoding = "UTF-8") %>%
+    xml_find_first("//d1:FakturaWierszCtrl") %>%
+    as_list() %>%
+    unlist() %>%
+    t() %>%
+    as.tibble() %>%
+    select(LiczbaWierszyFaktur) %>%
+    unlist() %>%
+    unname() %>%
+    as.numeric() -> JPK_FA_LiczbaWierszyFaktur
+
+
+  mydb <- dbConnect(RSQLite::SQLite(), "XML7.sqlite")
+
+RSQLite::dbConnect(RSQLite::SQLite(), file_SQL) -> myDB
+
+}
