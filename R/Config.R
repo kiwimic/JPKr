255bbe7e5104cc019abc3fc51391bc027fed4539
kiwimic <michalsiwik@gmail.com>
2018-06-22 22:52:21 +0200

    Przebudowa configu

--- R/Config.R
+++ R/Config.R
@@ -1,4 +1,9 @@
-JPK_CONFIG <- list(
-                  JPK_FA = list(Ctrl1 = c("JPK_FA_FakturaCtrl", "//d1:FakturaCtrl"),
-                                Ctrl2 = c("JPK_FA_FakturaWierszCtrl", "//d1:FakturaWierszCtrl"))
-                  )
+JPK_CONFIG <-
+  list(JPK_FA = list(
+    Ctrl = list(
+      FakturaCtrl = c("JPK_FA_FakturaCtrl", "//d1:FakturaCtrl"),
+      FakturaWierszCtrl = c("JPK_FA_FakturaWierszCtrl", "//d1:FakturaWierszCtrl")
+    ),
+    Tables = list(Faktura = "Faktura",
+                  FakturaWiersz = "FakturaWiersz")
+  ))
