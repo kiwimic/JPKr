550ddccf61e12b316c16706ec1880503be28a988
kiwimic <michalsiwik@gmail.com>
2018-06-22 22:45:46 +0200

    Funkcja do sum kontrolnych

--- R/getControlSum.R
+++ R/getControlSum.R
@@ -1,14 +1,14 @@
 
-#' getControlSum
+#' getControlSums
 #'
 #' @param pointer_of_file_xml Pointer to the xml file which was read with xml2::read_xml function
 #' @param xPath xpath for node for example "//d1:FakturaCtrl"
 #'
-#' @return value
+#' @return numeric tibble with controsums
 #' @export
 #'
 #' @examples
-getControlSum <- function(pointer_of_file_xml, xPath) {
+getControlSums <- function(pointer_of_file_xml, xPath) {
   if (sum(class(pointer_of_file_xml) %in% d2)==0) {
     stop(sprintf("pointer_of_file_xml needs to be class 'xml_document, xml_node', not %s", class(pointer_of_file_xml)))
   }
