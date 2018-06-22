9533f0c0b39c62c21745c5b25a559dca7c2dc8fa
kiwimic <michalsiwik@gmail.com>
2018-06-22 22:46:20 +0200

    Config do parametrów, jakie tabele są w jakich plikach JPK i czego szukać.

---
+++ R/Config.R
@@ -0,0 +1,4 @@
+JPK_CONFIG <- list(
+                  JPK_FA = list(Ctrl1 = c("JPK_FA_FakturaCtrl", "//d1:FakturaCtrl"),
+                                Ctrl2 = c("JPK_FA_FakturaWierszCtrl", "//d1:FakturaWierszCtrl"))
+                  )
