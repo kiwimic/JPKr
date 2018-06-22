96a55c09d698f6bed4347400ecc4427348ac8cfa
kiwimic <michalsiwik@gmail.com>
2018-06-22 22:59:04 +0200

    Typos, and param names.

--- R/getControlSum.R
+++ R/getControlSum.R
@@ -4,12 +4,12 @@
 #' @param pointer_of_file_xml Pointer to the xml file which was read with xml2::read_xml function
 #' @param xPath xpath for node for example "//d1:FakturaCtrl"
 #'
-#' @return numeric tibble with controsums
+#' @return numeric tibble with control sums
 #' @export
 #'
 #' @examples
 getControlSums <- function(pointer_of_file_xml, xPath) {
-  if (sum(class(pointer_of_file_xml) %in% d2)==0) {
+  if (sum(class(pointer_of_file_xml) %in% c("xml_document", "xml_node"))==0) {
     stop(sprintf("pointer_of_file_xml needs to be class 'xml_document, xml_node', not %s", class(pointer_of_file_xml)))
   }
   ret <- pointer_of_file_xml %>%
