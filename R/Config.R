126328b60c20c0acdca00ba46838f51a3fa934e1
kiwimic <michalsiwik@gmail.com>
2018-06-24 15:58:23 +0200

    Nowe elementy w configu

--- R/Config.R
+++ R/Config.R
@@ -4,6 +4,6 @@ JPK_CONFIG <-
       FakturaCtrl = c("JPK_FA_FakturaCtrl", "//d1:FakturaCtrl"),
       FakturaWierszCtrl = c("JPK_FA_FakturaWierszCtrl", "//d1:FakturaWierszCtrl")
     ),
-    Tables = list(Faktura = "Faktura",
+    Tables = list(Faktura = c("Faktura", "<Faktura typ=\"G\">", "</Faktura>"),
                   FakturaWiersz = "FakturaWiersz")
   ))
