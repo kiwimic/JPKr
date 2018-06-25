43bc77f2190ea3c0fc556f9ac3d0570f0d1bad2f
kiwimic <michalsiwik@gmail.com>
2018-06-25 19:12:04 +0200

    Removed old commented code.

--- R/BIG_XML_TO_SQLITE.R
+++ R/BIG_XML_TO_SQLITE.R
@@ -33,10 +33,6 @@ BIG_XML_TO_SQLITE <-
     ##TODO docelowo wprowadzić pętle po parametrze w configu
     ## W configu zaszyte są lokalizacje i nazwy zmiennych
     for (z in 1:length(JPK_CONFIG[[JPK_TYPE]][["Ctrl"]])) {
-      # assign(x = JPK_CONFIG_temp[["Ctrl"]][[i]][1],
-      #        value = getControlSums(JPK_UNKNOWN, xPath = JPK_CONFIG_temp[["Ctrl"]][[i]][2])
-      # )
-      # getControlSum(JPK_UNKNOWN, xPath = JPK_CONFIG[[JPK_TYPE]]$Ctrl[[i]][2])
       dbWriteTable(myDB,
                    JPK_CONFIG[[JPK_TYPE]]$Ctrl[[z]][1],
                    getControlSums(JPK_UNKNOWN, xPath = JPK_CONFIG[[JPK_TYPE]]$Ctrl[[z]][2]),
@@ -66,58 +62,4 @@ BIG_XML_TO_SQLITE <-
     ######XXXXXXXXXXXXXXXXXXXXXXXXXX#########
     ## 0.3 Wczytanie JPK jako text + Struktyryzacja#####
     ## 0.3.1 Wczytanie JPK ####
-  #   readLines(file_xml) %>%
-  #     paste(collapse = "\n") %>%
-  #     str_replace_all(pattern = ">(\\s+)?<", replacement = ">\n<") %>%
-  #     str_split(pattern = "\n") %>%
-  #     unlist() -> JPK_UNKNOWN
-  #
-  #   ## 0.3.2 Wyciągniecie indexów z wierszami faktur ####
-  #   index_ST <- grep(JPK_UNKNOWN, pattern = "<Faktura typ=\"G\">")
-  #   index_KC <- grep(JPK_UNKNOWN, pattern = "</Faktura>")
-  #   ## 0.3.2 Umieszczenie faktur w liscie####
-  #   ListaFaktur <- vector("list", length(index_ST))
-  #   for (i in 1:length(index_ST)) {
-  #     ListaFaktur[[i]] <-
-  #       paste(JPK_UNKNOWN[index_ST[i]:index_KC[i]], collapse = "\n")
-  #   }
-  #
-  #   ## 0.4 Export do SQLita ####
-  #   ## Jeśli chodzi o sumy kontrolne może tutaj iść pętla po liście do której wrzuce każda z sum kontrolnych
-  #
-  #   i <- 1
-  #   k <- 1
-  #   bufor <- vector("list", bufor_size)
-  #   for (i in 1:length(ListaFaktur)) {
-  #     ###0.4.3.1 Wczytanie pojedyńczego rekordu do ramki danych####
-  #     ListaFaktur[[i]] %>%
-  #       read_xml(encoding = "UTF-8") %>%
-  #       as_list() %>%
-  #       unlist() %>%
-  #       t() %>%
-  #       as.tibble() -> data
-  #
-  #     ###0.4.3.2 Dodanie brakujacych kolumn####
-  #     colnames(data) <-
-  #       str_replace(colnames(data),
-  #                   pattern = "^(Faktura\\.)",
-  #                   replacement = "")
-  #     data <-
-  #       AddMissingColsAndFillWith0(data, ALL_COLS = ALL_COLS_Faktura)
-  #
-  #     ###0.4.3.2 Wrzucenie pojeńczego rekordu do buforu####
-  #     bufor[[k]] <- data
-  #     k <- k + 1
-  #     ###0.4.3.3 Przygotowanie buforu do wrzutu do bazy danych####
-  #     if ((i %% bufor_size == 0) | (i == length(ListaFaktur))) {
-  #       Data <- bind_rows(bufor)
-  #       data <- convertCharColsToNum(data, guess = T)
-  #       dbWriteTable(myDB, "Faktury", Data, append = T)
-  #       bufor <- vector("list", bufor_size)
-  #       print(i)
-  #       k <- 1
-  #     }
-  #
-  #   }
-  # dbDisconnect(myDB)
   }
