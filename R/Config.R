afadc3e9c7a3f098abf73e55b095278c25f5e8d8
kiwimic <michalsiwik@gmail.com>
2018-06-25 19:17:34 +0200

    New config elements (now empty)

--- R/Config.R
+++ R/Config.R
@@ -4,12 +4,45 @@ JPK_CONFIG <-
       FakturaCtrl = c("FakturaCtrl", "//d1:FakturaCtrl"),
       FakturaWierszCtrl = c("FakturaWierszCtrl", "//d1:FakturaWierszCtrl")
     ),
-    Tables = list(Faktura = c("Faktura", "<Faktura typ=\"G\">", "</Faktura>", "^(Faktura\\.)"),
-                  FakturaWiersz = c("FakturaWiersz", "<FakturaWiersz typ=\"G\">","</FakturaWiersz>", "^(FakturaWiersz\\.)")
+    Tables = list(
+      Faktura = c(
+        "Faktura",
+        "<Faktura typ=\"G\">",
+        "</Faktura>",
+        "^(Faktura\\.)"
+      ),
+      FakturaWiersz = c(
+        "FakturaWiersz",
+        "<FakturaWiersz typ=\"G\">",
+        "</FakturaWiersz>",
+        "^(FakturaWiersz\\.)"
+      )
     ),
     Colnames = list(Faktura = ALL_COLS_Faktura,
                     FakturaWiersz = ALL_COLS_FakturaWiersz)
-    )
-    )
+  ),
+  JPK_VAT_2 = list(Ctrl = list(),
+                   Tables = list(),
+                   Colnames = list()),
+  JPK_VAT_3 = list(Ctrl = list(),
+                   Tables = list(),
+                   Colnames = list()),
+  JPK_MAG = list(Ctrl = list(),
+                 Tables = list(),
+                 Colnames = list()),
+  JPK_KR = list(Ctrl = list(),
+                 Tables = list(),
+                 Colnames = list()),
+  JPK_WB = list(Ctrl = list(),
+                 Tables = list(),
+                 Colnames = list()),
+  JPK_PKPIR = list(Ctrl = list(),
+                 Tables = list(),
+                 Colnames = list()),
+  JPK_EWP = list(Ctrl = list(),
+                 Tables = list(),
+                 Colnames = list())
+
+  )
 
 
