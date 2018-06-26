5925b605094c73a53abcf7cff65877ede76f31fd
kiwimic <michalsiwik@gmail.com>
2018-06-26 18:13:43 +0200

    Adding documentation and input validation.

---
+++ R/getPointerOfXML.r
@@ -0,0 +1,21 @@
+
+
+#' getPointerOfXML xml2::read_xml(x = file_xml)
+#'
+#' @param file_xml
+#'
+#' @return pointer to xml file
+#'
+#'
+#'
+getPointerOfXML <- function(file_xml = "") {
+  readit <- NULL
+  if (!is.character(file_xml)|is.na(file_xml)) {
+    stop("You have to pass path to xml file")
+  } else {
+    readit <- xml2::read_xml(x = file_xml)
+  }
+
+
+return(readit)
+}
