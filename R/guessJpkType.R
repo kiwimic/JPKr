7460ddb698b5f479b0dca660b7075af3b9e49b90
kiwimic <michalsiwik@gmail.com>
2018-06-26 11:17:05 +0200

    Dodany tryCatch

--- R/guessJpkType.R
+++ R/guessJpkType.R
@@ -12,15 +12,36 @@ getJpkType <- function(pointer_of_file_xml) {
   if (sum(class(pointer_of_file_xml) %in% c("xml_document", "xml_node"))==0) {
     stop(sprintf("pointer_of_file_xml needs to be class 'xml_document, xml_node', not %s", class(pointer_of_file_xml)))
   }
-  pointer_of_file_xml %>%
-    xml_find_first("//d1:Naglowek") %>%
-    as_list() %>%
-    unlist() %>%
-    t() %>%
-    as.tibble() %>%
-    select(KodFormularza) %>%
-    unlist() %>%
-    unname() -> TYP_PLIKU
 
-  return(TYP_PLIKU)
+ ret <- tryCatch({
+    pointer_of_file_xml %>%
+      xml_find_first("//d1:Naglowek") %>%
+      as_list() %>%
+      unlist() %>%
+      t() %>%
+      as.tibble() %>%
+      select(KodFormularza) %>%
+      unlist() %>%
+      unname() -> typJPK
+
+    return(typJPK)},
+    warning = function(w) {
+     pointer_of_file_xml %>%
+        xml_find_first("/tns:JPK/tns:Naglowek") %>%
+        as_list() %>%
+        unlist() %>%
+        t() %>%
+        as.tibble() %>%
+        select(KodFormularza) %>%
+        unlist() %>%
+        unname() -> typJPK
+      return(typJPK)
+    },
+    error = function(e) {
+      print(paste("X is no pointer to the JPK xml file", x))
+
+      NULL
+    }
+  )
+ return(ret)
 }
