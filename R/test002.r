84efc2f4cbf12dee01bdb6fba272c4b4cc779b09
kiwimic <michalsiwik@gmail.com>
2018-07-16 21:17:35 +0200

    Dodano Test002 i test001 dokumendacja, usuniętu validationofpolishnip, jest nowa funkcja zamiast niej isPolishNIP

---
+++ R/test002.r
@@ -0,0 +1,66 @@
+
+#' test002 Służy do obliczenia sum kontrolnych.
+#'
+#' @param data must be converted to numeric data.frame with JPK FA, or JPK_VAT
+#'
+#' @return a tibble with some metric: sum, median, mean and etc..
+#' @export
+#'
+#' @examples
+test002 <- function(data) {
+
+  data %>%
+    summarise(Razem = n()) %>%
+    mutate(Metric = "Liczba wierszy: ") -> LiczbaWierszy
+
+
+  data %>%
+    summarise_if(is.numeric, sum, na.rm = T) %>%
+    mutate(Metric = "Sumy: ") %>%
+    select(Metric, everything()) -> Sumy
+
+  data %>%
+    summarise_if(is.numeric, min, na.rm = T) %>%
+    mutate(Metric = "Min: ") %>%
+    select(Metric, everything()) -> Min
+
+  data %>%
+    summarise_if(is.numeric, max, na.rm = T) %>%
+    mutate(Metric = "Max: ") %>%
+    select(Metric, everything()) -> Max
+
+  data %>%
+    summarise_if(is.numeric, median, na.rm = T) %>%
+    mutate(Metric = "Median: ") %>%
+    select(Metric, everything()) -> Median
+
+  data %>%
+    summarise_if(is.numeric, quantile, probs = 0.025, na.rm = T) %>%
+    mutate(Metric = "Q025: ") %>%
+    select(Metric, everything()) -> Q025
+
+  data %>%
+    summarise_if(is.numeric, quantile, probs = 0.25, na.rm = T) %>%
+    mutate(Metric = "Q25: ") %>%
+    select(Metric, everything()) -> Q25
+
+  data %>%
+    summarise_if(is.numeric, quantile, probs = 0.75, na.rm = T) %>%
+    mutate(Metric = "Q75: ") %>%
+    select(Metric, everything()) -> Q75
+
+  data %>%
+    summarise_if(is.numeric, quantile, probs = 0.975, na.rm = T) %>%
+    mutate(Metric = "Q975: ") %>%
+    select(Metric, everything()) -> Q975
+
+  data %>%
+    apply(FUN = function(x) {return(sum(is.na(x)))}, 2) %>%
+    t() %>%
+    as.tibble() %>%
+    bind_cols(tibble(Metric = "NA")) %>%
+    select(Metric, everything()) ->  Empty
+
+  return(bind_rows(Sumy, Min, Max, Median, Q025, Q25, Q75, Q975, Empty, LiczbaWierszy))
+
+}
