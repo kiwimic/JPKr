a4cee859d3baa2ce6a8d3d234338a7ed87dc5852
kiwimic <michalsiwik@gmail.com>
2018-07-27 19:36:33 +0200

    Zbudowano test002 dla pliku JPK_FA

--- R/test002.r
+++ R/test002.r
@@ -1,12 +1,16 @@
 
-#' test002 Służy do zweryfikowania sum kontrolnych z pliku JPK
+#' test002 Sluzy do zweryfikowania sum kontrolnych z pliku JPK
 #'
 #' @param data must be converted to numeric data.frame with JPK FA, or JPK_VAT
 #'
 #' @return return list with 3 elements.
+#'
 #'  1. Logical TRUE, or FALSE depends on if control sums computed vs control sums from JPK file are equal,
+#'
 #'  2. Control sums from JPK file,
+#'
 #'  3. Control sums computed from 'data'
+#'
 #' @export
 #'
 #' @examples
@@ -25,17 +29,26 @@ test002 <- function(data = NA, controlSumsFromFile = NA, jpk_type = NA) {
     jpk_type,
     "JPK_VAT"="Good Morning",
     "JPK_FA"= {
-      data %>%
-        group_by(Species) %>%
-        summarise(N = n())
 
+    data %>%
+       select(P_13_1, P_13_2, P_13_3, P_13_4, P_13_5, P_13_6, P_13_7,
+              P_14_1, P_14_2, P_14_3, P_14_4, P_14_5) %>%
+       mutate_all(as.numeric) %>%
+       mutate(LiczbaFaktur = 1) %>%
+       summarise_all(sum, na.rm = T) %>%
+       mutate(WartoscFaktur = P_13_1 + P_13_2 + P_13_3 + P_13_4 + P_13_5 + P_13_6 + P_13_7 +
+                P_14_1 + P_14_2 + P_14_3, P_14_4 + P_14_5) %>%
+       mutate_all(round, 2) %>%
+       select(LiczbaFaktur, WartoscFaktur)
     },
     "JPK_WB"="Good Evening",
     "JPK_MAG"="Good Night"
 
   )
-
-  result <- list(result = FALSE,
+  if (identical(computedControlSums, controlSumsFromFile)) {
+    result_test <- TRUE
+  }
+  result <- list(result = result_test,
                  controlSumsFromFile = controlSumsFromFile,
                  computedControlSums = computedControlSums)
   return(result)
