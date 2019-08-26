2b7f5b7291cb19131ef9097bb2f0051c4ccae1f1
Siwik <michalsiwik@gmail.com>
2019-08-26 12:08:41 +0200

    Added installation docs, and simple script iniciation

---
+++ R/install.R
@@ -0,0 +1,18 @@
+##JPKr to oprogramowanie służące do wczytywania i analizy jednolitych plików kontrolnych - JPK ####
+
+###Instalacja#############
+##1. Należy zainstalować R, oraz RStudio
+##2. Skopiowanie katalogu JPKr do docelowego miejsca
+##3. Zainstalowanie wymaganych pakietów
+# dplyr (>= 0.7.0),
+# stringr(>= 1.2.0),
+# scales(>= 0.4.1),
+# tibble(>= 1.3.3),
+# writexl(>= 0.2),
+# xml2(>= 1.1.1),
+# RSQLite(>= 2.1.1),
+# readr(>= 1.1.1),
+# tidyr(>= 0.8.1)
+##4. Zbudowanie z plików źródlowych pakietu JPKr
+##5. Stworzenie zmiennej środowiskowej JPKr_GO_BINARY z lokalizacją do pliku JPK "jpk_bin.exe"
+
